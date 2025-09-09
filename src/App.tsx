01e6cdaf8a9cda1c79f0c8d9122690f0f14b4c3b
Leonardo Serrate <leonardoserrate9@gmail.com>
2025-09-09 12:05:20 -0400

    SI2-18 <Forgot Password>

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegisterPatientForm from "./pages/RegisterPatientForm";
 import Login from "./pages/Login";
 import Home from "./pages/Home";  // Importar el nuevo componente Home
+import ForgotPassword from "./pages/Forgot-Password.tsx";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
@@ -11,6 +13,8 @@ export default function App() {
         <Route path="/" element={<Home />} />  {/* Usar el componente Home */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<RegisterPatientForm />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/dashboard" element={<Dashboard/>} />
         <Route path="*" element={<div className="flex justify-center">Error 404</div>} />
       </Routes>
     </BrowserRouter>
